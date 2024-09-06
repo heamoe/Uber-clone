3bc5b02ee4e929363799f289569b3ce6eab35081
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-09-06 18:27:52 -0400

    initial upload

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, SafeAreaView } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to Uber Clone</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
