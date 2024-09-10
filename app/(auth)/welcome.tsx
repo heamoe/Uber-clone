29e3600886a7dc372e14ab3b2a9d2be25e101f33
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-09-10 16:14:24 -0400

    initial upload

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,12 @@
-import { SafeAreaView, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { router } from "expo-router";
 import Swiper from "react-native-swiper";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { onboarding } from "@/constants";
 
-const Onborading = () => {
+const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <SafeAreaView className="flex h-full items-center justify-center bg-white">
       <TouchableOpacity
@@ -13,10 +15,25 @@ const Onborading = () => {
       >
         <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
-      <Swiper ref={swiperRef} loop={false}></Swiper> dot =
-      {<view className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]" />}
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
 
-export default Onborading;
+export default Onboarding;
--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, SafeAreaView } from "react-native";
 import { Redirect } from "expo-router";
 
 const Home = () => {
-  return <Redirect href="/(auth)welcome" />;
+  return <Redirect href="/(auth)/welcome" />;
 };
 
 export default Home;
