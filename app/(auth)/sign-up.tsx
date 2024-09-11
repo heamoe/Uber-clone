e6642264c1bacb52d824fbd631458d52d386b245
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-09-11 13:24:58 -0400

    initial upload

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -7,8 +7,13 @@ const SignUp = () => {
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-          <Text className="text-2xl text-black font-JakartaSemiBold absolute buttom-0 left-5">
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
             Create Your Account
+          </Text>
+        </View>
+        <View className="p-5">
+          <Text className="text-lg text-black font-JosefinSans font-medium mb-5">
+            Full Name
           </Text>
         </View>
       </View>
