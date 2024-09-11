f14d9bf0f6a491ac6e0c73b45396cee47e076f3a
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-09-11 09:44:34 -0400

    initial upload

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,18 @@
-import { SafeAreaView, Text } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { images } from "@/constants";
 
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text>Sign Up</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute buttom-0 left-5">
+            Create Your Account
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
