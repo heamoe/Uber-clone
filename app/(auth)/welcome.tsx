9effd1c360fe36af94dad61301f15e2dc29a15ea
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-09-10 21:22:15 -0400

    initial upload

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -9,10 +9,12 @@ import { router } from "expo-router";
 import Swiper from "react-native-swiper";
 import { useRef, useState } from "react";
 import { onboarding } from "@/constants";
+import CustomButton from "@/components/CustomButton";
 
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
   return (
     <SafeAreaView className="flex h-full items-center justify-center bg-white">
       <TouchableOpacity
@@ -49,6 +51,15 @@ const Onboarding = () => {
           </View>
         ))}
       </Swiper>
+      <CustomButton
+        title={isLastSlide ? "Skip" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10"
+      />
     </SafeAreaView>
   );
 };
