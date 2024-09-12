9062c1fc539a54792ddbddf5155b90a364889422
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-09-12 17:20:41 -0400

    initial upload

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,7 +1,15 @@
 import { Image, ScrollView, Text, View } from "react-native";
-import { images } from "@/constants";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputField";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
@@ -12,9 +20,13 @@ const SignUp = () => {
           </Text>
         </View>
         <View className="p-5">
-          <Text className="text-lg text-black font-JosefinSans font-medium mb-5">
-            Full Name
-          </Text>
+          <InputField
+            label="Name"
+            placeholder="Enter name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
         </View>
       </View>
     </ScrollView>
