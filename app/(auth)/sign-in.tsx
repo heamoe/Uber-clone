dbfc364654bb86ea89e291a27776f0b04c4cb811
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-09-16 09:33:01 -0400

    updated

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -8,29 +8,20 @@ import OAuth from "@/components/OAuth";
 
 const SignIn = () => {
   const [form, setForm] = useState({
-    name: "",
     email: "",
     password: "",
-    confirmPassword: "",
   });
-  const onSignUpPress = async () => {};
+  const onLogInPress = async () => {};
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
           <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
-            Create Your Account
+            Welcome👋
           </Text>
         </View>
         <View className="p-5">
-          <InputField
-            label="Name"
-            placeholder="Enter name"
-            icon={icons.person}
-            value={form.name}
-            onChangeText={(value) => setForm({ ...form, name: value })}
-          />
           <InputField
             label="Email"
             placeholder="Enter name"
@@ -46,28 +37,18 @@ const SignIn = () => {
             secureTextEntry={true}
             onChangeText={(value) => setForm({ ...form, password: value })}
           />
-          <InputField
-            label="Confirm Password"
-            placeholder="Enter name"
-            icon={icons.lock}
-            value={form.confirmPassword}
-            secureTextEntry={true}
-            onChangeText={(value) =>
-              setForm({ ...form, confirmPassword: value })
-            }
-          />
           <CustomButton
-            title="Sign Up"
-            onPress={onSignUpPress}
+            title="Log In"
+            onPress={onLogInPress}
             className="mt-6"
           />
           <OAuth />
           <Link
-            href="/sign-in"
+            href="/sign-up"
             className="text-lg text-center text-general-200 mt-10"
           >
-            <Text>Already have an account?</Text>
-            <Text className="text-primary-100">Sign In</Text>
+            <Text>Don't have a account?</Text>
+            <Text className="text-primary-100">Sign Up</Text>
           </Link>
         </View>
       </View>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,11 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { Slot } from "expo-router";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -22,6 +27,11 @@ export default function RootLayout() {
     "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
     "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
   });
+  if (!publishableKey) {
+    throw new Error(
+      "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env",
+    );
+  }
 
   useEffect(() => {
     if (loaded) {
@@ -34,11 +44,15 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
