2e7b35f1fc991f73dbcc7d5103dd6a90666cbb04
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-09-28 16:57:04 -0400

    updated

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from "expo-router";
 import { Image, ImageSourcePropType, View } from "react-native";
+
 import { icons } from "@/constants";
 
 const TabIcon = ({
@@ -12,7 +13,9 @@ const TabIcon = ({
   <View
     className={`flex flex-row justify-center items-center rounded-full ${focused ? "bg-general-300" : ""}`}
   >
-    <View>
+    <View
+      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? "bg-general-400" : ""}`}
+    >
       <Image
         source={source}
         tintColor="white"
@@ -22,47 +25,71 @@ const TabIcon = ({
     </View>
   </View>
 );
-const layout = () => (
-  <Tabs initial="index" screenOptions={{ tabBarActiveTintColor: "white" }}>
-    <Tabs.Screen
-      name="home"
-      options={{
-        title: "Home",
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <TabIcon focused={focused} source={icons.home} />
-        ),
+
+export default function Layout() {
+  return (
+    <Tabs
+      initialRouteName="index"
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#333333",
+          borderRadius: 50,
+          paddingBottom: 0, // ios only
+          overflow: "hidden",
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 78,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          position: "absolute",
+        },
       }}
-    />
-    <Tabs.Screen
-      name="rides"
-      options={{
-        title: "Rides",
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <TabIcon focused={focused} source={icons.list} />
-        ),
-      }}
-    />
-    <Tabs.Screen
-      name="chat"
-      options={{
-        title: "Chat",
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <TabIcon focused={focused} source={icons.chat} />
-        ),
-      }}
-    />
-    <Tabs.Screen
-      name="profile"
-      options={{
-        title: "Profile",
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (
-          <TabIcon focused={focused} source={icons.profile} />
-        ),
-      }}
-    />
-  </Tabs>
-);
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.home} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="rides"
+        options={{
+          title: "Rides",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.list} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: "Chat",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.chat} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.profile} focused={focused} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
