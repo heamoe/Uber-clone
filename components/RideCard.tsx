862f1798f21ab3f0221574ad17c9d24cbabecffc
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-10-08 17:28:41 -0400

    update

---
+++ components/RideCard.tsx
@@ -0,0 +1,9 @@
+import { Ride } from "@/types/type";
+import { View, Text } from "react-native";
+
+const RideCard = ({ ride }: { ride: Ride }) => (
+  <View>
+    <Text>{ride.driver.first_name}</Text>
+  </View>
+);
+export default RideCard;
