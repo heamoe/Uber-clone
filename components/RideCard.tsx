eb78ce5e4e8f9d21428cf9fd31937181a50b4f67
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-10-08 17:42:53 -0400

    update

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,9 +1,22 @@
 import { Ride } from "@/types/type";
 import { View, Text } from "react-native";
 
-const RideCard = ({ ride }: { ride: Ride }) => (
+const RideCard = ({
+  ride: {
+    destination_latitude,
+    destination_longitude,
+    destination_address,
+    driver,
+    orrign_address,
+    created_at,
+    ride_time,
+    payment_status,
+  },
+}: {
+  ride: Ride;
+}) => (
   <View>
-    <Text>{ride.driver.first_name}</Text>
+    <Text>{driver.first_name}</Text>
   </View>
 );
 export default RideCard;
