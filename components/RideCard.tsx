5ca6b37d140be7107045cdbcb1c578a6e49598e1
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-10-09 12:54:29 -0400

    update

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,5 +1,6 @@
 import { Ride } from "@/types/type";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { icons } from "@/constants";
 
 const RideCard = ({
   ride: {
@@ -15,8 +16,27 @@ const RideCard = ({
 }: {
   ride: Ride;
 }) => (
-  <View>
-    <Text>{driver.first_name}</Text>
+  <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
+    <View className="flex flex-row items-center justify-center p-3">
+      <View className="flex flex-row items-center justify-between">
+        <Image
+          source={{
+            uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+          }}
+          className="w-[80px] h-[90px] rounded-lg"
+        />
+        <View className="flex flex-col mx-5 gap-y-5 flex-1">
+          <View className="flex flex-row items-center gap-x-2">
+            <Image source={icons.to} className="w-5 h-5" />
+            <Text className="text-md font-JakartaMedium">
+              {destination_address}
+            </Text>
+          </View>
+        </View>
+      </View>
+      <Text className="text-3xl">{driver.first_name}</Text>
+    </View>
   </View>
 );
+
 export default RideCard;
