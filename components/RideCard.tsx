d4e82edd0d1cefb38588a6f43230679f89c565d9
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-10-09 13:18:58 -0400

    update

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,6 +1,7 @@
 import { Ride } from "@/types/type";
 import { View, Text, Image } from "react-native";
 import { icons } from "@/constants";
+import { formatDate, formatTime } from "@/lib/utils";
 
 const RideCard = ({
   ride: {
@@ -8,7 +9,7 @@ const RideCard = ({
     destination_longitude,
     destination_address,
     driver,
-    orrign_address,
+    origin_address,
     created_at,
     ride_time,
     payment_status,
@@ -17,7 +18,7 @@ const RideCard = ({
   ride: Ride;
 }) => (
   <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
-    <View className="flex flex-row items-center justify-center p-3">
+    <View className="flex flex-col items-center justify-center p-3">
       <View className="flex flex-row items-center justify-between">
         <Image
           source={{
@@ -28,13 +29,29 @@ const RideCard = ({
         <View className="flex flex-col mx-5 gap-y-5 flex-1">
           <View className="flex flex-row items-center gap-x-2">
             <Image source={icons.to} className="w-5 h-5" />
-            <Text className="text-md font-JakartaMedium">
+            <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+              {origin_address}
+            </Text>
+          </View>
+          <View className="flex flex-row items-center gap-x-2">
+            <Image source={icons.point} className="w-5 h-5" />
+            <Text className="text-md font-JakartaMedium" numberOfLines={1}>
               {destination_address}
             </Text>
           </View>
         </View>
       </View>
-      <Text className="text-3xl">{driver.first_name}</Text>
+
+      <View className="flex flex-cal w-full mt-5 bg-general-500 rounded-lg p-3 item-start justify-center">
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+          <Text className="text-md font-JakartaMedium text-gray-500">
+            Date & Time
+          </Text>
+          <Text className="text-md font-JakartaMedium text-gray-500">
+            {formatDate(created_at)}, {formatTime(ride_time)}
+          </Text>
+        </View>
+      </View>
     </View>
   </View>
 );
