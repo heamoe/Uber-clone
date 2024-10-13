17d0b96cfdaaab11c42a9a586db69e1e8dbd35a6
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-10-13 17:15:48 -0400

    update

--- components/Map.tsx
+++ components/Map.tsx
@@ -2,8 +2,21 @@ import { Text } from "react-native";
 import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 const Map = () => {
+  /*const region = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };*/
   return (
-    <MapView provider={PROVIDER_DEFAULT} className="w-full h-full rounded-2xl">
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showPointsOfInterest={false}
+      //initialRegion={{}}
+    >
       <Text>Map</Text>
     </MapView>
   );
