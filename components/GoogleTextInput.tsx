2e5cede2bc46f19d75641b8a01b658c5cb547636
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-10-13 10:09:34 -0400

    update

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,7 +1,16 @@
 import { View, Text } from "react-native";
+import { GoogleInputProps } from "@/types/type";
 
-const GoogleTextInput = () => (
-  <View>
+const GoogleTextInput = ({
+  icon,
+  initialLocation,
+  containerStyle,
+  textInputBackgroundColor,
+  handlePress,
+}: GoogleInputProps) => (
+  <View
+    className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}
+  >
     <Text>Search</Text>
   </View>
 );
