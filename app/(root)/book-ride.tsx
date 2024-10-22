88c10c95ebaf572cdf7e69b0c67c48d5644c5c10
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-10-22 18:27:43 -0400

    update

---
+++ app/(root)/book-ride.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const BookRide = () => {
+  return (
+    <View className="flex flex-col w-full items-center justify-center mt-10">
+      <Text className="font-bold">Book Ride</Text>
+    </View>
+  );
+};
+
+export default BookRide;
