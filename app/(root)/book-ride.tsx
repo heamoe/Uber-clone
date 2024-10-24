e97a7f9b90fd62ce96cf5fda712ed189139ee2a2
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-10-23 22:28:09 -0400

    update

--- app/(root)/book-ride.tsx
+++ app/(root)/book-ride.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const BookRide = () => {
   return (
     <View className="flex flex-col w-full items-center justify-center mt-10">
-      <Text className="font-bold">Book Ride</Text>
+      <Text className="font-bold">Book Ride！</Text>
     </View>
   );
 };
