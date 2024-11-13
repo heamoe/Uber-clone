a2ce7b65fd42f619e02cb58dcf8b021e36f37cb5
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-11-13 14:09:20 -0500

    update

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -27,7 +27,9 @@ const InputField = ({
         <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>
           {label}
         </Text>
-        <View className="flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}">
+        <View
+          className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}
+        >
           {icon && (
             <Image source={icon} className="w-6 h-6 ml-4 ${iconStyle}" />
           )}
